b4f0a67b89467b67f1ce1b31ff4a040981d323a9
SemkivVitaly <91784593+SemkivVitaly@users.noreply.github.com>
2021-10-29 00:59:52 +0300

    Add files via upload

---
+++ exam7-a.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define n_max 100
+
+int n;
+int mmax, mmin;
+int mark[n_max];
+
+#define mmin 2
+#define mmax 5
+
+#define mpass 4.6f
+
+int main()
+{
+    printf("n: ");
+    scanf("%d", &n);
+	
+	printf("Введите оценки:");
+	
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &mark[i]);
+		if(mark[i]<mmin)
+		{
+		printf("Error\n");
+		return 0;
+		}
+		if(mark[i]>mmax)
+		{
+		printf("Error\n");
+		return 0;
+		}
+	}
+	
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+    	sum += mark[i];
+		
+    float avg = (float) sum / (float) n;
+
+    printf("avg: %.1f\n", avg);
+    if (avg >= mpass) printf("yes\n");
+    else printf("no\n");
+
+    return 0;
+	
+
+}
