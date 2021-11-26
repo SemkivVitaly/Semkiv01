7cb2df157c2d035417bced78d8bf57330bc5db28
SemkivVitaly <91784593+SemkivVitaly@users.noreply.github.com>
2021-11-26 19:59:03 +0300

    Update exam12-b.c

--- exam12-b.c
+++ exam12-b.c
@@ -13,68 +13,21 @@ int c1[m_max][n_max];
 int c2[m_max][n_max];
 
 int n, m;
-char spc[100];
 
-void addition()
+void input()
 {
-	for (int y = 0; y < m; y++)
-		for (int x = 0; x < n; x++)
-		{
-			c1[y][x] = a[y][x] + b[y][x];
-			
-		}
-	
-		printf("\n%sc1%s\n", spc, spc);
-		
-		for (int y = 0; y < m; y++)		
-		{
-			for (int x = 0; x < n; x++)
-				printf("% 4d", c1[y][x]);
-			
-			printf("\n");
-		}
-}
-
-void subtraction()
-{
-	for (int y = 0; y < m; y++)
-		for (int x = 0; x < n; x++)
-		{
-			c2[y][x] = a[y][x] - b[y][x];		
-		}
-	
-		printf("\n%sc2%s\n", spc, spc);
-		
-		for (int y = 0; y < m; y++)		
-		{
-			for (int x = 0; x < n; x++)
-				printf("% 4d", c2[y][x]);
-			
-			printf("\n");
-		}
-}
-
-int main()
-{
-	
-	
-	
-	printf("n (cols), m (rows): ");
-	scanf("%d %d", &n, &m);
-	
-	
-	
-	
-	
 	for (int y = 0; y < m; y++)
 		for (int x = 0; x < n; x++)
 		{
 			a[y][x] = vmin + rand() % (vmax - vmin + 1);
 			b[y][x] = vmin + rand() % (vmax - vmin + 1);
 		}
-	
-		
-		for (int i = 0; i < 100; i++)
+}
+
+void output()
+{
+	char spc[100];
+	for (int i = 0; i < 100; i++)
 			spc[i] = ' ';
 		spc[n * 4 / 2] = 0;
 		
@@ -96,14 +49,66 @@ int main()
 			printf("\n");
 		}
 		
+		printf("\n%sc1%s\t%sc2\n", spc, spc, spc);
+		
+		for (int y = 0; y < m; y++)		
+		{
+			for (int x = 0; x < n; x++)
+				printf("% 4d", c1[y][x]);
+				
+			printf("\t");
+			
+			for (int x = 0; x < n; x++)
+			{
+				printf("% 4d", c2[y][x]);
+
+			}
+			
+			printf("\n");
+		}
+}
+
+void addition()
+{
+	for (int y = 0; y < m; y++)
+		for (int x = 0; x < n; x++)
+		{
+			c1[y][x] = a[y][x] + b[y][x];
+			
+		}
+	
+		
+}
+
+void subtraction()
+{
+	for (int y = 0; y < m; y++)
+		for (int x = 0; x < n; x++)
+		{
+			c2[y][x] = a[y][x] - b[y][x];		
+		}
+	
+	
+}
+
+int main()
+{
+	
+	printf("n (cols), m (rows): ");
+	scanf("%d %d", &n, &m);
+	
+	
+		
 int choice = -1; 
 
 do
  {
 	printf("\n");
 	printf("0 - exit\n");
-	printf("1 - addition\n");
-    printf("2 - subtraction\n");
+	printf("1 - input matrixA and MatrixB\n");
+	printf("2 - addition\n");
+    printf("3 - subtraction\n");
+	printf("4 - output\n");
 
 	printf("\n");
 	
@@ -115,9 +120,11 @@ do
 	switch(choice)
 	{
 		case 0: return 0; break;	
-		case 1: addition(); break;
-		case 2: subtraction(); break;
-     
+		case 1: input(); break;
+		case 2: addition(); break;
+		case 3: subtraction(); break;
+		case 4: output(); break;
+		
 	}
     printf("\n");
 	printf("press");
