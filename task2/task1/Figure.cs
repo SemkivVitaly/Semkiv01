cf2a60ca872dc013f31c470a4eab32c513f7bddb
SemkivVitaly <91784593+SemkivVitaly@users.noreply.github.com>
2021-12-15 14:44:12 +0300

    Add files via upload

---
+++ task2/task1/Figure.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task2
+{
+    public class Figure
+    {
+        public Vec2 pos = new Vec2();
+        public RGB color = RGB.getRandom(); //а будет ли работать?
+        public bool selected;
+        public int numInList; 
+        public float setX;
+        public string nameFigure;
+        
+
+        virtual public bool test(float x, float y)
+        {
+            return false;
+        }
+
+        virtual public void draw(Graphics g)
+        {
+            
+        }
+
+        virtual public string setName()
+        {
+            return nameFigure;
+        }
+
+        virtual public float sdf(Vec2 p)
+        {
+            float innerD = 0; float outerD = 0;
+            return innerD + outerD;
+        }
+
+
+    }
+}
